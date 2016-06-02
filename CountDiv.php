06f6ee3c25bc1f9d7c5250083e45907ce9d26c49
Andrew Wippl <ajwippl@gmail.com>
2016-06-01 21:39:00 -0400

    Create CountDiv.php
    Write a function:
    
    function solution($A, $B, $K);
    that, given three integers A, B and K, returns the number of integers within the range [A..B] that are divisible by K, i.e.:
    
    { i : A ≤ i ≤ B, i mod K = 0 }
    For example, for A = 6, B = 11 and K = 2, your function should return 3, because there are three numbers divisible by 2 within the range [6..11], namely 6, 8 and 10.
    
    Assume that:
    
    A and B are integers within the range [0..2,000,000,000];
    K is an integer within the range [1..2,000,000,000];
    A ≤ B.
    Complexity:
    
    expected worst-case time complexity is O(1);
    expected worst-case space complexity is O(1).

---
+++ CountDiv.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Codility;
+
+class CountDiv 
+{
+  function solution($A, $B, $K) {
+      if($A % $K == 0) {
+        return (int) floor(($B - $A) / $K) + 1;
+      } else {
+        return (int) floor($B / $K - (floor($A / $K) + 1)) + 1;
+      }
+  }
+}  
+/*
+Write a function:
+
+function solution($A, $B, $K);
+that, given three integers A, B and K, returns the number of integers within the range [A..B] that are divisible by K, i.e.:
+
+{ i : A ≤ i ≤ B, i mod K = 0 }
+For example, for A = 6, B = 11 and K = 2, your function should return 3, because there are three numbers divisible by 2 within the range [6..11], namely 6, 8 and 10.
+
+Assume that:
+
+A and B are integers within the range [0..2,000,000,000];
+K is an integer within the range [1..2,000,000,000];
+A ≤ B.
+Complexity:
+
+expected worst-case time complexity is O(1);
+expected worst-case space complexity is O(1).
+*/
